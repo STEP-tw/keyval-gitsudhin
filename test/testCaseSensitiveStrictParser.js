f1812b3a58cd5e8170a6084de9e510ac68dd8d83
Sudhin <sudhinmachad@gmail.com>
2018-01-08 15:53:50 +0530

    Added test for mixed case,key with numbers and special characters

--- test/testCaseSensitiveStrictParser.js
+++ test/testCaseSensitiveStrictParser.js
@@ -13,6 +13,27 @@ describe("strict parser that is case insensitive",function(){
     let parsed=kvParser.parse("NAME=jayanth");
     assert.deepEqual(parsed,expected);
   });
+  it("should parse when specified keys are in both lower and uppercase and actual is not",function(){
+    let kvParser=new StrictParser(["name"],false);
+    let expected=new Parsed();
+    expected["NamE"]="jayanth";
+    let parsed=kvParser.parse("NamE=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should throw error when specified keys having special characters",function(){
+    let kvParser=new StrictParser(["name"],false);
+    let expected=new Parsed();
+    assert.throws(()=>{
+      kvParser.parse("_NAME=jayanth");
+    })
+  });
+  it("should throw error when specified keys having numbers",function(){
+    let kvParser=new StrictParser(["name"],false);
+    let expected=new Parsed();
+    assert.throws(()=>{
+      kvParser.parse("NAME123=jayanth");
+    })
+  });
 });
 
 describe("strict parser that is case sensitive",function(){
