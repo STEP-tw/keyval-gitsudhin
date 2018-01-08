10ce42053d8041b16451e4cbcb896ca729c62389
Sudhin <sudhinmachad@gmail.com>
2018-01-08 15:40:20 +0530

    Added caseSensitive key handling feature

--- src/strictParseInfo.js
+++ src/strictParseInfo.js
@@ -2,21 +2,25 @@ const Parsed=require("./parsed.js");
 const ParseInfo=require("./parseInfo.js");
 const InvalidKeyError=require("./errors/invalidKeyError.js");
 
-const contains=function(list,key) {
+const contains=function(list,key,caseSensitive) {
   return list.find(function(validKey){
+    if(!caseSensitive){
+      key=key.toLowerCase();
+    }
     return key==validKey;
   });
 }
 
-var StrictParseInfo=function(initialParsingFunction,validKeys) {
+var StrictParseInfo=function(initialParsingFunction,validKeys,caseSensitive) {
   ParseInfo.call(this,initialParsingFunction);
   this.validKeys=validKeys;
+  this.caseSensitive=caseSensitive;
 }
 
 StrictParseInfo.prototype=Object.create(ParseInfo.prototype);
 
 StrictParseInfo.prototype.pushKeyValuePair=function() {
-  if(!contains(this.validKeys,this.currentKey))
+  if(!contains(this.validKeys,this.currentKey,this.caseSensitive))
     throw new InvalidKeyError("invalid key",this.currentKey,this.currentPos);
   this.parsedKeys[this.currentKey]=this.currentValue;
   this.resetKeysAndValues();
